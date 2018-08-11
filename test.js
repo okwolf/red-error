715e89d8fc8165da6a919ca5eeb9e78e12cc3e11
Wolfgang <wolf@okwolf.com>
2018-08-11 14:16:21 -0700

    es 6

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-const spawnSync = require("child_process").spawnSync;
+const { spawnSync } = require("child_process");
 const assert = require("assert");
 const formatRed = require("./formatRed.js");
 
